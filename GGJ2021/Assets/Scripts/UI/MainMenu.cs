1dd17c9a2a24d8fe5a1a6a8a5357e4ee7753eb54
Stefan Lopuszanski <Stexe@UConn.edu>
2021-01-28 19:00:47 -0500

    Main Menu
    More files for basic stuff and TextMeshPro

---
+++ GGJ2021/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject titlePanel;
+    public GameObject selectionPanel;
+
+    public void StartGame()
+    {
+        StartCoroutine(BeginGame(1.1f));
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Main");
+        Application.Quit();
+        Debug.Log("Game quit!");
+    }
+
+    IEnumerator BeginGame(float waitTime)
+    {
+        for (float i = 0; i <= .25; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        titlePanel.SetActive(false);
+        selectionPanel.SetActive(false);
+        SceneManager.LoadScene("Main");
+    }	
+}
---
+++ GGJ2021/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject pausePanel;
+	private bool isPaused;
+
+	void Update ()
+    {
+        if (Input.GetButtonDown ("Cancel") && !isPaused) 
+		{
+			DoPause();
+		}
+		else if (Input.GetButtonDown ("Cancel") && isPaused) 
+		{
+			UnPause ();
+		}
+	}
+
+	public void DoPause()
+	{
+		isPaused = true;
+		Time.timeScale = 0.0f;
+        pausePanel.SetActive(true);
+    }
+
+	public void UnPause()
+	{
+		isPaused = false;
+		Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
+	}
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Main");
+        Application.Quit();
+    }
+
+}
