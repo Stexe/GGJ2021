b8a6fe1276ab1606ff92c69d457d045d61511652
Adam Reifsneider <adamreifsneider@gmail.com>
2021-01-29 21:09:53 -0500

    can move pieces around

--- GGJ2021/Assets/Scripts/PieceDragging.cs
+++ GGJ2021/Assets/Scripts/PieceDragging.cs
@@ -15,28 +15,60 @@ public class PieceDragging : MonoBehaviour
 
     private void Update()
     {
-        Vector2 currentMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (heldPiece != null)
         {
             heldPiece.transform.position = new Vector3(
-                heldPiece.transform.position.x + currentMouse.x - previousMousePosition.x,
-                heldPiece.transform.position.y + currentMouse.y - previousMousePosition.y,
+                heldPiece.transform.position.x + currentMousePosition.x - previousMousePosition.x,
+                heldPiece.transform.position.y + currentMousePosition.y - previousMousePosition.y,
                 // float above other pieces
                 -1);
         }
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             //stop floating above other pieces
-            heldPiece.transform.position = new Vector3(heldPiece.transform.position.x, heldPiece.transform.position.y, 0);
+            heldPiece.transform.position = new Vector3(currentHoldingSquare.transform.position.x, currentHoldingSquare.transform.position.y, 0);
             
             heldPiece = null;
         }
-        previousMousePosition = currentMouse;
+        previousMousePosition = currentMousePosition;
+
+        // swap pieces
+        var hoveredSquare = GetHoveredSquare();
+        if (heldPiece != null && hoveredSquare != null && hoveredSquare.name != currentHoldingSquare.name)
+        {
+            Debug.Log("SWAPPING " + hoveredSquare.name + ", " + currentHoldingSquare.name);
+            SwapPieces(currentHoldingSquare, hoveredSquare);
+        }
     }
 
-    void OnSquareClicked(BoardSquare square)
+    private void OnSquareClicked(BoardSquare square)
     {
         currentHoldingSquare = square;
         heldPiece = square.Piece;
+    }
+
+    private BoardSquare GetHoveredSquare()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider == null)
+        {
+            return null;
+        }
+
+        return hit.collider.GetComponent<BoardSquare>();
+    }
+
+    private void SwapPieces(BoardSquare squareWithHeldPiece, BoardSquare squareWithNotHeldPiece)
+    {
+        //move ownership of not-held-piece
+        var notHeldPiece = squareWithNotHeldPiece.Piece;
+        notHeldPiece.transform.position = squareWithHeldPiece.transform.position;
+        squareWithHeldPiece.Piece = notHeldPiece;
+
+        //move ownership of held piece
+        squareWithNotHeldPiece.Piece = heldPiece;
+        currentHoldingSquare = squareWithNotHeldPiece;
     }
 }
