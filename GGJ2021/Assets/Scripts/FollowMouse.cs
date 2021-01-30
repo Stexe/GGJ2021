89bdbba3643a87326521a373e44799e00377c184
Stefan Lopuszanski <Stexe@UConn.edu>
2021-01-30 14:53:16 -0500

    Res Test Hookup

---
+++ GGJ2021/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+
+    public Canvas masterCanvas;
+    public PieceDragging pieceDragging;
+    private EnergyBarToolkit.FilledRendererUGUI filledRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pieceDragging = pieceDragging.GetComponent<PieceDragging>();
+        filledRenderer = filledRenderer.GetComponent<EnergyBarToolkit.FilledRendererUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pieceDragging.heldPiece == null)
+        {
+            filledRenderer.enabled = false;
+        }
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(masterCanvas.transform as RectTransform, Input.mousePosition, masterCanvas.worldCamera, out pos);
+        transform.position = masterCanvas.transform.TransformPoint(pos);
+    }
+}
--- GGJ2021/Assets/Scripts/PieceDragging.cs
+++ GGJ2021/Assets/Scripts/PieceDragging.cs
@@ -6,7 +6,7 @@ public class PieceDragging : MonoBehaviour
 {
     private Board board;
 
-    private Piece heldPiece;
+    public Piece heldPiece;
     private BoardSquare currentHoldingSquare;
     private Vector2 previousMousePosition;
 
