a52f6511d077f8b2693cdc887e6a3ecac8f23e81
Stefan Lopuszanski <Stexe@UConn.edu>
2021-01-31 10:10:28 -0500

    Building Icons and Names

---
+++ GGJ2021/Assets/Scripts/ResourceEfficiency.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResourceEfficiency : MonoBehaviour
+{
+    public PieceType type;
+    private EnergyBar energyBar;
+    private int amount;
+
+    private void Awake()
+    {
+        energyBar = GetComponentInParent<EnergyBar>();
+    }
+
+    private void Start()
+    {
+        amount = energyBar.valueMax;
+        GetComponent<TextMeshProUGUI>().text = amount.ToString();
+    }
+
+    public void IncreaseAmount(int byAmount)
+    {
+        amount += byAmount;
+        energyBar.valueMax += byAmount;
+        GetComponent<TextMeshProUGUI>().text = amount.ToString();
+    }
+
+    public void DecreaseAmount(int byAmount)
+    {
+        amount -= byAmount;
+        energyBar.valueMax -= byAmount;
+        GetComponent<TextMeshProUGUI>().text = amount.ToString();
+    }
+}
